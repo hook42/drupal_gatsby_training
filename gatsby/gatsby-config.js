8c726f93701f3130bbdc0d3e1334893aba235fc3
Ryan Bateman <rybateman@gmail.com>
2019-02-09 10:01:26 -0900

    Initial state for 02: drupal/graphql

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -28,6 +28,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    /*{
+      resolve: `gatsby-source-drupal`,
+      options: {
+				// This is the address of our Drupal docker container - not the address exposed to our client machine.
+				// If you were working with Gatsby on your local machine with a remote Drupal site, you could point
+				// it directly to it's web address.
+        baseUrl: `http://drupal/`,
+        apiBase: `jsonapi`,
+      },
+    },*/
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
