0e04efb326a92d4657de9b9d9155bc6cdd835fea
Ryan Bateman <rybateman@gmail.com>
2019-02-11 16:33:30 -0900

    linter fixes

--- gatsby/src/components/recipe-teaser.js
+++ gatsby/src/components/recipe-teaser.js
@@ -2,12 +2,10 @@ import React from "react"
 import { Link } from "gatsby"
 import { withPrefix } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const RecipeTeaser = ({imgSrc, recipeTitle, recipeDate, recipeSummary}) => (
   <article>
-    <img src={withPrefix('drupal-files/' + imgSrc)} />
+    <img src={withPrefix('drupal-files/' + imgSrc)} alt="" />
     <h2>{recipeTitle}</h2>
     <p>{recipeDate}</p>
     <p>{recipeSummary}</p>
